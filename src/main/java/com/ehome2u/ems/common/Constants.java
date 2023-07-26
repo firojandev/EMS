1855b555cf9e2a60ebc7e768fdb9382dc521f887
Md Altaf Hoshain <altaf@squaregroup.com>
2023-07-26 16:38:04 +0600

    1st commit

---
+++ src/main/java/com/ehome2u/ems/common/Constants.java
@@ -0,0 +1,11 @@
+package com.ehome2u.ems.common;
+
+public class Constants {
+    public static final String SUCCESSFUL_LOGIN = "Login successful!";
+    public static final String SUCCESSFUL_FETCH = "Fetched successfully!";
+
+    //400 : Bad request : login
+    public static final String INPUT_ERROR = "User credentials required.";
+
+
+}
