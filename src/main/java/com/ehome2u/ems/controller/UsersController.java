9bc8dd530a6b233ad68abf7b1c0e76c5261a7c18
Md Altaf Hoshain <altaf@squaregroup.com>
2023-07-27 12:52:43 +0600

    Users listing is ongoing

---
+++ src/main/java/com/ehome2u/ems/controller/UsersController.java
@@ -0,0 +1,16 @@
+package com.ehome2u.ems.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("users")
+public class UsersController {
+    @GetMapping
+   public String index(){
+
+       return "users/index";
+   }
+
+}
