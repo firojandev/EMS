abad14ac9d38a53340aee8d2232bf423607f44a6
Md Altaf Hoshain <altaf@squaregroup.com>
2023-07-27 13:15:22 +0600

    Users listing is done

--- src/main/java/com/ehome2u/ems/controller/UsersController.java
+++ src/main/java/com/ehome2u/ems/controller/UsersController.java
@@ -1,15 +1,22 @@
 package com.ehome2u.ems.controller;
 
+import com.ehome2u.ems.services.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("users")
 public class UsersController {
-    @GetMapping
-   public String index(){
 
+    @Autowired
+    UserServiceImpl mUserServiceImpl;
+
+    @GetMapping
+   public String index(Model model){
+        model.addAttribute("list", mUserServiceImpl.getAll());
        return "users/index";
    }
 
