59517e6dfda119fc0334638aab45c12cc454b86f
Md Altaf Hoshain <altaf@squaregroup.com>
2023-07-29 09:15:18 +0600

    authenticated user name is displayed

--- src/main/java/com/ehome2u/ems/controller/DashboardController.java
+++ src/main/java/com/ehome2u/ems/controller/DashboardController.java
@@ -1,12 +1,18 @@
 package com.ehome2u.ems.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DashboardController {
   @GetMapping("/dashboard")
-  public String dashboard(){
+  public String dashboard(ModelMap model){
+      Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+      String name = auth.getName(); //get logged in username
+      model.addAttribute("name", name);
       return "dashboard";
   }
 }
